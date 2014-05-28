a5d3e5f6ff4f6225d535c6249e31100768bb8929
Marty Rosenberg <mrosenberg@mozilla.com>
2014-05-28 12:26:52 -0400

    aaand add in the new headers that were created

---
+++ js/src/jit/arm64/BaselineHelpers-arm64.h
@@ -0,0 +1,132 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sts=4 et sw=4 tw=99:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef jit_arm_BaselineHelpers_arm_h
+#define jit_arm_BaselineHelpers_arm_h
+
+#ifdef JS_ION
+#include "jit/BaselineFrame.h"
+#include "jit/BaselineIC.h"
+#include "jit/BaselineRegisters.h"
+#include "jit/IonMacroAssembler.h"
+
+namespace js {
+namespace jit {
+
+// Distance from sp to the top Value inside an IC stub (no return address on the stack on ARM).
+static const size_t ICStackValueOffset = 0;
+
+inline void
+EmitRestoreTailCallReg(MacroAssembler &masm)
+{
+    // No-op on ARM because link register is always holding the return address.
+}
+
+inline void
+EmitRepushTailCallReg(MacroAssembler &masm)
+{
+    // No-op on ARM because link register is always holding the return address.
+}
+
+inline void
+EmitCallIC(CodeOffsetLabel *patchOffset, MacroAssembler &masm)
+{
+    JS_ASSERT(0 && "EmitCallIC");
+}
+
+inline void
+EmitEnterTypeMonitorIC(MacroAssembler &masm,
+                       size_t monitorStubOffset = ICMonitoredStub::offsetOfFirstMonitorStub())
+{
+JS_ASSERT(0 && "EmitEnterTypeMonitorIC");
+}
+
+inline void
+EmitReturnFromIC(MacroAssembler &masm)
+{
+    JS_ASSERT(0 && "EmitReturnFromIC");
+}
+
+inline void
+EmitChangeICReturnAddress(MacroAssembler &masm, Register reg)
+{
+    JS_ASSERT(0 && "EmitChangeICReturnAddress");
+}
+
+inline void
+EmitTailCallVM(JitCode *target, MacroAssembler &masm, uint32_t argSize)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+inline void
+EmitCreateStubFrameDescriptor(MacroAssembler &masm, Register reg)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+inline void
+EmitCallVM(JitCode *target, MacroAssembler &masm)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+// Size of vales pushed by EmitEnterStubFrame.
+static const uint32_t STUB_FRAME_SIZE = 4 * sizeof(void *);
+static const uint32_t STUB_FRAME_SAVED_STUB_OFFSET = sizeof(void *);
+
+inline void
+EmitEnterStubFrame(MacroAssembler &masm, Register scratch)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+inline void
+EmitLeaveStubFrame(MacroAssembler &masm, bool calledIntoIon = false)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+inline void
+EmitStowICValues(MacroAssembler &masm, int values)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+inline void
+EmitUnstowICValues(MacroAssembler &masm, int values, bool discard = false)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+inline void
+EmitCallTypeUpdateIC(MacroAssembler &masm, JitCode *code, uint32_t objectOffset)
+{
+    JS_ASSERT(R2 == ValueOperand(r0));
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+template <typename AddrType>
+inline void
+EmitPreBarrier(MacroAssembler &masm, const AddrType &addr, MIRType type)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+inline void
+EmitStubGuardFailure(MacroAssembler &masm)
+{
+    JS_ASSERT(0 && "EmitTailCallVM");
+}
+
+
+} // namespace jit
+} // namespace js
+
+#endif // JS_ION
+
+#endif /* jit_arm_BaselineHelpers_arm_h */
+
---
+++ js/src/jit/arm64/BaselineRegisters-arm64.h
@@ -0,0 +1,63 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sts=4 et sw=4 tw=99:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef jit_arm64_BaselineRegisters_arm64_h
+#define jit_arm64_BaselineRegisters_arm64_h
+
+#ifdef JS_ION
+
+#include "jit/IonMacroAssembler.h"
+
+namespace js {
+namespace jit {
+
+// r15 = program-counter
+// r14 = link-register
+
+// r13 = stack-pointer
+// r11 = frame-pointer
+static MOZ_CONSTEXPR_VAR Register BaselineFrameReg {Registers::x11};
+static MOZ_CONSTEXPR_VAR Register BaselineStackReg {Registers::sp};
+
+// ValueOperands R0, R1, and R2.
+// R0 == JSReturnReg, and R2 uses registers not
+// preserved across calls.  R1 value should be
+// preserved across calls.
+static MOZ_CONSTEXPR_VAR Register R0_{Registers::x1};
+static MOZ_CONSTEXPR_VAR Register R1_{Registers::x2};
+static MOZ_CONSTEXPR_VAR Register R2_{Registers::x0};
+static MOZ_CONSTEXPR_VAR ValueOperand R0(R0_);
+static MOZ_CONSTEXPR_VAR ValueOperand R1(R1_);
+static MOZ_CONSTEXPR_VAR ValueOperand R2(R2_);
+
+// BaselineTailCallReg and BaselineStubReg
+// These use registers that are not preserved across
+// calls.
+static MOZ_CONSTEXPR_VAR Register BaselineTailCallReg {Registers::x30};
+static MOZ_CONSTEXPR_VAR Register BaselineStubReg     {Registers::x9};
+
+static MOZ_CONSTEXPR_VAR Register ExtractTemp0        {Registers::x3 };
+static MOZ_CONSTEXPR_VAR Register ExtractTemp1        {Registers::x4 };
+
+// Register used internally by MacroAssemblerARM.
+static MOZ_CONSTEXPR_VAR Register BaselineSecondScratchReg {Registers::x6};
+
+// R7 - R9 are generally available for use within stubcode.
+
+// Note that BaselineTailCallReg is actually just the link
+// register.  In ARM code emission, we do not clobber BaselineTailCallReg
+// since we keep the return address for calls there.
+
+// FloatReg0 must be equal to ReturnFloatReg.
+static MOZ_CONSTEXPR_VAR FloatRegister FloatReg0   {FloatRegisters::v0};
+static MOZ_CONSTEXPR_VAR FloatRegister FloatReg1   {FloatRegisters::v1};
+
+} // namespace jit
+} // namespace js
+
+#endif // JS_ION
+
+#endif /* jit_arm_BaselineRegisters_arm_h */
