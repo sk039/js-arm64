5ab3e16ec93b9c4a98635dd145f87a6aa2c3f03f
Sean Stangl <sstangl@mozilla.com>
2014-09-30 14:54:59 -0700

    Fix build.

--- js/src/jit/arm64/vixl/MacroAssembler-vixl.cpp
+++ js/src/jit/arm64/vixl/MacroAssembler-vixl.cpp
@@ -770,13 +770,13 @@ MacroAssemblerVIXL::Push(const CPURegister& src0, const CPURegister& src1,
     //   str x28, [x28, #-8]!
     // which pre-decrements the stack pointer (x28 in this case) by 8,
     // and then stores the decremented value.
-    VIXL_ASSERT(!src0.Is(masm.GetStackPointer()));
+    VIXL_ASSERT(!src0.Is(GetStackPointer()));
     VIXL_ASSERT(!src0.Is(sp));
-    VIXL_ASSERT(!src1.Is(masm.GetStackPointer()));
+    VIXL_ASSERT(!src1.Is(GetStackPointer()));
     VIXL_ASSERT(!src1.Is(sp));
-    VIXL_ASSERT(!src2.Is(masm.GetStackPointer()));
+    VIXL_ASSERT(!src2.Is(GetStackPointer()));
     VIXL_ASSERT(!src2.Is(sp));
-    VIXL_ASSERT(!src3.Is(masm.GetStackPointer()));
+    VIXL_ASSERT(!src3.Is(GetStackPointer()));
     VIXL_ASSERT(!src3.Is(sp));
 
     int count = 1 + src1.IsValid() + src2.IsValid() + src3.IsValid();
