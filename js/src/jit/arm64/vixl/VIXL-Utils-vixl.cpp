ea6c94cd75e89fb346fab7b1d775395bc03034b0
Sean Stangl <sstangl@mozilla.com>
2014-12-09 13:58:47 -0800

    Reformat VIXL-*.

--- js/src/jit/arm64/vixl/VIXL-Utils-vixl.cpp
+++ js/src/jit/arm64/vixl/VIXL-Utils-vixl.cpp
@@ -1,5 +1,5 @@
-// -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-// vim: set ts=8 sts=2 et sw=2 tw=99:
+// -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vim: set ts=8 sts=4 et sw=4 tw=99:
 //
 // Copyright 2013, ARM Limited
 // All rights reserved.
@@ -36,72 +36,79 @@
 namespace js {
 namespace jit {
 
-uint32_t float_to_rawbits(float value) {
-  uint32_t bits = 0;
-  memcpy(&bits, &value, 4);
-  return bits;
+uint32_t
+float_to_rawbits(float value)
+{
+    uint32_t bits = 0;
+    memcpy(&bits, &value, 4);
+    return bits;
 }
 
 
-uint64_t double_to_rawbits(double value) {
-  uint64_t bits = 0;
-  memcpy(&bits, &value, 8);
-  return bits;
+uint64_t
+double_to_rawbits(double value)
+{
+    uint64_t bits = 0;
+    memcpy(&bits, &value, 8);
+    return bits;
 }
 
-
-float rawbits_to_float(uint32_t bits) {
-  float value = 0.0;
-  memcpy(&value, &bits, 4);
-  return value;
+float
+rawbits_to_float(uint32_t bits)
+{
+    float value = 0.0;
+    memcpy(&value, &bits, 4);
+    return value;
 }
 
-
-double rawbits_to_double(uint64_t bits) {
-  double value = 0.0;
-  memcpy(&value, &bits, 8);
-  return value;
+double
+rawbits_to_double(uint64_t bits)
+{
+    double value = 0.0;
+    memcpy(&value, &bits, 8);
+    return value;
 }
 
-
-int CountLeadingZeros(uint64_t value, int width) {
-  MOZ_ASSERT((width == 32) || (width == 64));
-  int count = 0;
-  uint64_t bit_test = UINT64_C(1) << (width - 1);
-  while ((count < width) && ((bit_test & value) == 0)) {
-    count++;
-    bit_test >>= 1;
-  }
-  return count;
+int
+CountLeadingZeros(uint64_t value, int width)
+{
+    MOZ_ASSERT((width == 32) || (width == 64));
+    int count = 0;
+    uint64_t bit_test = UINT64_C(1) << (width - 1);
+    while ((count < width) && ((bit_test & value) == 0)) {
+        count++;
+        bit_test >>= 1;
+    }
+    return count;
 }
 
-
-int CountLeadingSignBits(int64_t value, int width) {
-  MOZ_ASSERT((width == 32) || (width == 64));
-  if (value >= 0) {
-    return CountLeadingZeros(value, width) - 1;
-  } else {
+int
+CountLeadingSignBits(int64_t value, int width)
+{
+    MOZ_ASSERT((width == 32) || (width == 64));
+    if (value >= 0)
+        return CountLeadingZeros(value, width) - 1;
     return CountLeadingZeros(~value, width) - 1;
-  }
 }
 
-
-int CountTrailingZeros(uint64_t value, int width) {
-  MOZ_ASSERT((width == 32) || (width == 64));
-  int count = 0;
-  while ((count < width) && (((value >> count) & 1) == 0)) {
-    count++;
-  }
-  return count;
+int
+CountTrailingZeros(uint64_t value, int width)
+{
+    MOZ_ASSERT((width == 32) || (width == 64));
+    int count = 0;
+    while ((count < width) && (((value >> count) & 1) == 0))
+        count++;
+    return count;
 }
 
+int
+CountSetBits(uint64_t value, int width)
+{
+    MOZ_ASSERT((width == 32) || (width == 64));
 
-int CountSetBits(uint64_t value, int width) {
-  MOZ_ASSERT((width == 32) || (width == 64));
-
-  if (width == 32)
-    return mozilla::CountPopulation32(uint32_t(value));
-  return mozilla::CountPopulation64(value);
+    if (width == 32)
+        return mozilla::CountPopulation32(uint32_t(value));
+    return mozilla::CountPopulation64(value);
 }
 
 uint64_t LowestSetBit(uint64_t value) {
