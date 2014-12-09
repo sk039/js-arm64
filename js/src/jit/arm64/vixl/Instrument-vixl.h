5ef9b82e6ca7f15e3d8d1e3d1ccb9a81c21d639a
Sean Stangl <sstangl@mozilla.com>
2014-12-09 12:29:39 -0800

    Reformat Instrument-vixl.h.

--- js/src/jit/arm64/vixl/Instrument-vixl.h
+++ js/src/jit/arm64/vixl/Instrument-vixl.h
@@ -1,5 +1,5 @@
-// -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-// vim: set ts=8 sts=2 et sw=2 tw=99:
+// -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vim: set ts=8 sts=4 et sw=4 tw=99:
 //
 // Copyright 2013, ARM Limited
 // All rights reserved.
@@ -41,68 +41,66 @@ namespace jit {
 const int kCounterNameMaxLength = 256;
 const uint64_t kDefaultInstrumentationSamplingPeriod = 1 << 22;
 
-
 enum InstrumentState {
-  InstrumentStateDisable = 0,
-  InstrumentStateEnable = 1
+    InstrumentStateDisable = 0,
+    InstrumentStateEnable = 1
 };
-
 
 enum CounterType {
-  Gauge = 0,      // Gauge counters reset themselves after reading.
-  Cumulative = 1  // Cumulative counters keep their value after reading.
+    Gauge = 0,      // Gauge counters reset themselves after reading.
+    Cumulative = 1  // Cumulative counters keep their value after reading.
 };
 
+class Counter
+{
+  public:
+    Counter(const char* name, CounterType type = Gauge);
 
-class Counter {
- public:
-  Counter(const char* name, CounterType type = Gauge);
+    void Increment();
+    void Enable();
+    void Disable();
+    bool IsEnabled();
+    uint64_t count();
+    const char* name();
+    CounterType type();
 
-  void Increment();
-  void Enable();
-  void Disable();
-  bool IsEnabled();
-  uint64_t count();
-  const char* name();
-  CounterType type();
-
- private:
-  char name_[kCounterNameMaxLength];
-  uint64_t count_;
-  bool enabled_;
-  CounterType type_;
+  private:
+    char name_[kCounterNameMaxLength];
+    uint64_t count_;
+    bool enabled_;
+    CounterType type_;
 };
 
+class Instrument : public DecoderVisitor
+{
+  public:
+    explicit Instrument(const char* datafile = NULL,
+            uint64_t sample_period = kDefaultInstrumentationSamplingPeriod);
+    ~Instrument();
 
-class Instrument: public DecoderVisitor {
- public:
-  explicit Instrument(const char* datafile = NULL,
-    uint64_t sample_period = kDefaultInstrumentationSamplingPeriod);
-  ~Instrument();
+    void Enable();
+    void Disable();
 
-  void Enable();
-  void Disable();
+    // Declare all Visitor functions.
+    #define DECLARE(A) void Visit##A(Instruction* instr);
+    VISITOR_LIST(DECLARE)
+    #undef DECLARE
 
-  // Declare all Visitor functions.
-  #define DECLARE(A) void Visit##A(Instruction* instr);
-  VISITOR_LIST(DECLARE)
-  #undef DECLARE
+  private:
+    void Update();
+    void DumpCounters();
+    void DumpCounterNames();
+    void DumpEventMarker(unsigned marker);
+    void HandleInstrumentationEvent(unsigned event);
+    Counter* GetCounter(const char* name);
 
- private:
-  void Update();
-  void DumpCounters();
-  void DumpCounterNames();
-  void DumpEventMarker(unsigned marker);
-  void HandleInstrumentationEvent(unsigned event);
-  Counter* GetCounter(const char* name);
+    void InstrumentLoadStore(Instruction* instr);
+    void InstrumentLoadStorePair(Instruction* instr);
 
-  void InstrumentLoadStore(Instruction* instr);
-  void InstrumentLoadStorePair(Instruction* instr);
+    std::list<Counter*> counters_;
 
-  std::list<Counter*> counters_;
-
-  FILE *output_stream_;
-  uint64_t sample_period_;
+    FILE *output_stream_;
+    uint64_t sample_period_;
 };
 
 } // namespace jit
