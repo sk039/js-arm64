786683a7f5e56f22982c1d7dc724c665e7b72cd8
Marty Rosenberg <mrosenberg@mozilla.com>
2015-03-10 09:53:53 -0400

    Fix our GetStackPointer to not assert

--- js/src/jit/arm64/vixl/MacroAssembler-vixl.h
+++ js/src/jit/arm64/vixl/MacroAssembler-vixl.h
@@ -976,7 +976,12 @@ class MacroAssemblerVIXL : public Assembler
         return sp_;
     }
     const Register GetStackPointer_() const {
-        return Register::FromCode(sp_.code());
+        int code = sp_.code();
+        if (code == kSPRegInternalCode) {
+            code = 31;
+        }
+        return Register::FromCode(code);
+        
     }
     CPURegList* TmpList() { return &tmp_list_; }
     CPURegList* FPTmpList() { return &fptmp_list_; }
